f414e46a3200d296e5fd73b596e628ca1b08aab6
TigranZakharyan <zakharyantigran27@gmail.com>
2023-11-25 23:35:11 +0400

    resolved dark mode bug2

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -14,7 +14,7 @@ const Modal = (props: Props) => {
 						<div className="relative bg-white rounded-lg shadow p-10 dark:bg-gray-700 max-w-[500px] w-full mx-auto">
 								{
 									props?.onClose && (
-										<button type="button" className="absolute top-2 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={props.onClose}>
+										<button type="button" className="absolute top-2 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" onClick={props.onClose}>
 											<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
 													<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
 											</svg>
